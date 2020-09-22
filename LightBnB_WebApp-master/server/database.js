c22f49bb892e6a571efc422d1e4d07caee9a1a8b
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-09-22 22:34:52 +0000

    implemant addProperty that add a new propertie, and fixe getAllProperties to include also properties that have not a review LEFT JOIN

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -96,22 +96,14 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
- /*
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
-  .then(res => {
-    return res.rows;
-  });
-*/
+
   // 1
   const queryParams = [];
   // 2
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
 
   // 3
@@ -161,7 +153,7 @@ const getAllProperties = function(options, limit = 10) {
   `;
 
   // 5
-  console.log(queryString, queryParams);
+  //console.log(queryString, queryParams);
 
   // 6
   return pool.query(queryString, queryParams)
@@ -177,9 +169,33 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  console.log(property);
+  let queryString = `
+  INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *;
+    `;
+  return pool.query(queryString, 
+    [
+      property.title,
+      property.description,
+      property.owner_id,
+      property.cover_photo_url,
+      property.thumbnail_photo_url,
+      parseInt(property.cost_per_night) * 100,
+      parseInt(property.parking_spaces),
+      parseInt(property.number_of_bathrooms),
+      parseInt(property.number_of_bedrooms),
+      true,
+      property.province,
+      property.city,
+      property.country,
+      property.street,
+      property.post_code
+    ]
+    )
+    .then(res => {
+      return res.rows.length > 0 ? res.rows[0] : NULL;
+  });
 }
 exports.addProperty = addProperty;
